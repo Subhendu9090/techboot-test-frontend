892201e19ab019187762914bb3c12cd4ed5b7c2f
“subhendu <subhendujena38@gmail.com>
2025-02-01 11:19:10 +0530

    login page design change

--- src/pages/PublicPages/Login.tsx
+++ src/pages/PublicPages/Login.tsx
@@ -9,8 +9,8 @@ import { useAuth } from '../../contexts/Store';
 import { Alert } from '../../utils/Alert';
 
 const Login: React.FC = () => {
-  const navigate = useNavigate(); 
-  const {login} =useAuth()
+  const navigate = useNavigate();
+  const { login } = useAuth();
   const [formData, setFormData] = useState({
     email: 'subhendu.jena@qwegle.com',
     password: '123456789',
@@ -68,19 +68,19 @@ const Login: React.FC = () => {
     setLoading(true);
 
     try {
-      const userCredential : any = await signInWithEmailAndPassword(
+      const userCredential: any = await signInWithEmailAndPassword(
         auth,
         formData.email,
         formData.password
       );
-      
+
       login(userCredential.user.email, userCredential?.user?.accessToken);
-      
+
       console.log('Logged in user:', userCredential?.user);
       navigate(paths.overview);
     } catch (error: any) {
       console.log('Login Error', error);
-      setAlertMessage(error.code || " Something went Wrong ");
+      setAlertMessage(error.code || ' Something went Wrong ');
       setShowAlert(true);
     } finally {
       setLoading(false);
@@ -89,68 +89,84 @@ const Login: React.FC = () => {
 
   return (
     <>
-    <div className="flex flex-col-reverse items-center justify-center min-h-screen bg-white md:flex-row ">
-      {/* Left Side - Login Form */}
-      <div className="relative w-full md:w-2/4">
-        <div className="w-full px-4 py-8 md:px-8">
-          <form
-            onSubmit={handleSubmit}
-            className="max-w-md  p-6 mx-auto bg-[#F1F1F1] md:py-16 rounded-xl"
-          >
-            <h2 className="mb-6 text-2xl font-bold text-gray-800">
-              Get Started
-            </h2>
+      <div className="flex flex-col-reverse items-center justify-center min-h-screen bg-white md:flex-row ">
+        {/* Left Side - Login Form */}
+        <div className="relative w-full md:w-2/4">
+          <div className="w-full px-4 py-8 md:px-8">
+            <form
+              onSubmit={handleSubmit}
+              className="max-w-md  p-6 mx-auto bg-[#F1F1F1] md:py-16 rounded-xl"
+            >
+              <h2 className="mb-6 text-2xl font-bold text-gray-800">
+                Get Started
+              </h2>
 
-            {/* Input fields */}
-            {inputFields.map((field) => (
-              <div key={field.name} className="mb-8">
-                <CustomInput
-                  name={field.name}
-                  label={field.label}
-                  type={field.type}
-                  value={formData[field.name as keyof typeof formData]}
-                  errorText={field.errorText}
-                  onChange={handleInputChange}
-                  onBlur={handleBlur}
-                />
-              </div>
-            ))}
+              {/* Input fields */}
+              {inputFields.map((field) => (
+                <div key={field.name} className="mb-8">
+                  <CustomInput
+                    name={field.name}
+                    label={field.label}
+                    type={field.type}
+                    value={formData[field.name as keyof typeof formData]}
+                    errorText={field.errorText}
+                    onChange={handleInputChange}
+                    onBlur={handleBlur}
+                  />
+                </div>
+              ))}
 
-            {/* Forgot Password Link */}
-            <div className="mb-6 text-right">
-              <Link
-                to={paths.forgotPassword}
-                className="text-sm text-blue-600 hover:text-blue-700 hover:underline"
-              >
-                Forgot Password?
-              </Link>
-            </div>
-
-            {/* Submit Button */}
+              {/* Submit Button */}
               <CustomButton
-              fullWidth={true}
+                fullWidth={true}
                 loading={loading}
                 label="Login"
                 type="submit"
                 size="medium"
               />
-          </form>
+
+              <div className='flex justify-between w-full p-1 '>
+                <div className="flex space-x-2">
+                  <input
+                    type="checkbox"
+                    id="remember"
+                    className="w-4 h-4 text-blue-700 border-gray-300 rounded focus:ring-blue-500"
+                  />
+                  <label
+                    htmlFor="remember"
+                    className="text-sm text-[#0E1E2B] cursor-pointer font-normal"
+                  >
+                    Remember Me
+                  </label>
+                </div>
+
+                {/* Forgot Password Link */}
+                <div className="mb-6 text-right">
+                  <Link
+                    to={paths.forgotPassword}
+                    className="text-sm text-blue-600 hover:text-blue-700 hover:underline"
+                  >
+                    Forgot Password?
+                  </Link>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        {/* Right Side - Image */}
+        <div className="relative w-full min-h-[300px] md:h-screen md:w-2/4">
+          <img
+            src="LoginPage/Bike.svg"
+            alt="Background"
+            className="object-cover w-full h-full"
+          />
         </div>
       </div>
-
-      {/* Right Side - Image */}
-      <div className="relative w-full min-h-[300px] md:h-screen md:w-2/4">
-        <img
-          src="LoginPage/Bike.svg"
-          alt="Background"
-          className="object-cover w-full h-full"
-        />
-      </div>
-    </div>
-    {showAlert && (
-        <Alert 
-          message={alertMessage} 
-          type="error" 
+      {showAlert && (
+        <Alert
+          message={alertMessage}
+          type="error"
           duration={5000}
           onClose={() => setShowAlert(false)}
         />
